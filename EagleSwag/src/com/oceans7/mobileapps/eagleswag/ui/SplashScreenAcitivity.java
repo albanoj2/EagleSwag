e2cde7af36871336a7cab4d55c99a580d8b8658f
Justin Albano <albano.justin@gmail.com>
2013-05-30 12:56:01 -0400

    Renamed splash page activity

--- EagleSwag/src/com/oceans7/mobileapps/eagleswag/ui/SplashScreenAcitivity.java
+++ EagleSwag/src/com/oceans7/mobileapps/eagleswag/ui/SplashScreenAcitivity.java
@@ -6,18 +6,18 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class LoadingAcitivity extends Activity {
+public class SplashScreenAcitivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loading);
+        setContentView(R.layout.activity_splash_screen);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_loading, menu);
+        getMenuInflater().inflate(R.menu.activity_splash_screen, menu);
         return true;
     }
     
