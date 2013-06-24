397977a151aeb477f2d0d188c683259446dfb996
Justin Albano <albano.justin@gmail.com>
2013-06-23 23:37:45 -0400

    Moved score controller test cases

--- EagleSwagTests/src/com/oceans7/mobile/eagleswag/test/userdata/ScoreControllerTest.java
+++ EagleSwagTests/src/com/oceans7/mobile/eagleswag/test/userdata/ScoreControllerTest.java
@@ -16,7 +16,7 @@
  * <http://www.gnu.org/licenses/>
  */
 
-package com.oceans7.mobile.eagleswag.test.domain;
+package com.oceans7.mobile.eagleswag.test.userdata;
 
 import android.content.Context;
 import android.test.InstrumentationTestCase;
